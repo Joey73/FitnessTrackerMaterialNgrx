33314a9486267b75932811d0f979f1472e01ca1b
Joey73 <schmitz873@gmail.com>
2018-04-19 18:01:57 +0200

    Changed to allow prod-deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA0yPQ2lcYAQ7mxdexqApe8KjBJMi-0il4",
+    authDomain: "ng-fitness-tracker-6b8c5.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-6b8c5.firebaseio.com",
+    projectId: "ng-fitness-tracker-6b8c5",
+    storageBucket: "ng-fitness-tracker-6b8c5.appspot.com",
+    messagingSenderId: "1082895866278"
+  }  
 };
